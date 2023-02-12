c49181adb63426b0e244c9690fd4dd97b07e7d6e
shmup <srdjan.mprivate@gmail.com>
2023-02-12 17:43:30 +0100

    Added functionality to OPT3001

--- OPT3001.c
+++ OPT3001.c
@@ -7,23 +7,28 @@
 static void set_fs_mode(void){
     /*0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0*/
     /*1100000000000000*/
-    int16_t current_reg_val = I2C1_Read2ByteRegister(OPT3001_DEV_ID, OPT3001_CONFIG);
+    int16_t current_reg_val = I2C1_Read2ByteRegister(OPT3001_DEV_ID, OPT3001_CONFIG_REG);
     int16_t fs_reg = current_reg_val | 0xC000;
-    I2C1_Write2ByteRegister(OPT3001_DEV_ID, OPT3001_CONFIG, fs_reg);
+    I2C1_Write2ByteRegister(OPT3001_DEV_ID, OPT3001_CONFIG_REG, fs_reg);
 }
 
 int16_t get_data(void){
-    int16_t lumen = I2C1_Read2ByteRegister(OPT3001_DEV_ID, OPT3001_RESULT);
+    int16_t lumen = I2C1_Read2ByteRegister(OPT3001_DEV_ID, OPT3001_RESULT_REG);
     return lumen;
 }
 /*  Returns the calculated value in lux */
-double calculate_lux(void){
-    double lux = 0;
-    int16_t value = get_data();
+float calculate_lux(void){
+    float lux = 0.0;
+    int16_t register_value = get_data();
+    
+    uint16_t exponent = register_value >> 12;
+    exponent = (1 << exponent);     // 1 << N is same as 2^N
 
-    int8_t exp = value >> 12;
-    int16_t frac = value & 0x0FFF;
+    float lsb_size = 0.01 * exponent;
 
-    lux = 0.01 * (1 << exp) * frac;
-    return lux;  
+    uint16_t remain = register_value << 4;   //WHAT?!
+    remain = remain >> 4;
+
+    float lux = lsb_size * remain;
+    return lux;
 }
--- OPT3001.h
+++ OPT3001.h
@@ -3,16 +3,13 @@
 
 #include <stdint.h>
 
-#define OPT3001_RESULT      0x00
-#define OPT3001_CONFIG      0x01
-/*  Manufacturer ID   */
-#define OPT3001_MANF_ID     0x7E
-/*  Device ID   */
-#define OPT3001_DEV_ID      0x7F
+#define OPT3001_RESULT_REG      0x00
+#define OPT3001_CONFIG_REG      0x01
+#define OPT3001_DEV_ID          0x7F
 
-double calculate_lux(void);
+float calculate_lux(void);
 void config_sensor(void);
 uint16_t get_data(void);
 
 
-#endif
+#endif  //OPT3001_H
